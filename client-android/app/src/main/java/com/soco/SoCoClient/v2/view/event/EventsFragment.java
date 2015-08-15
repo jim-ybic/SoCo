dd2acfdeea57935a5f31020d8bb90f59545fc9b2
jim <jim.ybic@gmail.com>
2015-08-15 17:05:15 +0800

    save new event

--- client-android/app/src/main/java/com/soco/SoCoClient/v2/view/event/EventsFragment.java
+++ client-android/app/src/main/java/com/soco/SoCoClient/v2/view/event/EventsFragment.java
@@ -452,6 +452,7 @@ public class EventsFragment extends Fragment implements View.OnClickListener {
         Log.v(tag, "create new event");
         Event e = new Event(getActivity().getApplicationContext());
         e.setName(name);
+        e.addContext(context);
         e.save();
 
         DataLoader dataLoader = new DataLoader(context);
