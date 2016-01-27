4ea283ba93db429d226413e0233d7112e828f31c
jim-ybic <jim.ybic@gmail.com>
2016-01-27 15:31:39 +0800

    logo for e-payment (paypal, alipay); join event screen close button;

--- client-android/app/src/main/java/com/soco/SoCoClient/events/details/JoinEventActivity.java
+++ client-android/app/src/main/java/com/soco/SoCoClient/events/details/JoinEventActivity.java
@@ -42,28 +42,6 @@ public class JoinEventActivity extends ActionBarActivity implements TaskCallBack
         edt.execute(event_id);
 
     }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_activity_join_event, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 
     public void close(View view){
         finish();
